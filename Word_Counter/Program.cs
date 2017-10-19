71ff46ba411938d7a9099a374a4f36a25da8593b
Andrii <taboroffsky6@gmail.com>
2017-10-19 09:49:58 +0300

    Add project files.

---
+++ Word_Counter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Word_Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
