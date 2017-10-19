073b2148bdfbb5db6b20096b1aa814e810aa817a
Andrii <taboroffsky6@gmail.com>
2017-10-19 12:18:28 +0300

    initial

---
+++ Word_Counter/WordCounter.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections;
+
+namespace Word_Counter
+{
+    public class WordCounter
+    {
+        public static void Main(string[] args)
+        {
+            //Receiving data from a command line
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Argument not found.");
+                return;
+            }
+
+            string rawText = string.Empty;
+
+            try
+            {
+                rawText = File.ReadAllText(args[0]);
+            }
+            catch
+            {
+                Console.WriteLine("An error occured while reading from a file.");
+            }
+
+            //Splitting the whole text into an array of words
+            IEnumerable<string> words = SplitText(rawText);
+
+            //Hashtable provides faster access to each word rather than looping through an array.
+            Hashtable wordsTable = CreateWordsHashTable(words);
+
+            //Displaying rsults into a console.
+            foreach (DictionaryEntry item in wordsTable)
+            {
+                Console.WriteLine(item.Key.ToString() + ' ' + item.Value);
+            }
+
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Splits the text into a word array. Words with dashes are considered to be a single word.
+        /// </summary>
+        /// <param name="rawText">Text to be splitted</param>
+        /// <returns>Lower case words array</returns>
+        public static IEnumerable<string> SplitText(string rawText)
+        {
+            string pattern = "(?!-)[A-Za-z-]+";
+            MatchCollection matches = Regex.Matches(rawText, pattern);
+
+            foreach (Match item in matches)
+            {
+                yield return item.Value.ToLower();
+            }
+        }
+
+        /// <summary>
+        ///Counts an occurrence of each word in a colletion.
+        /// </summary>
+        /// <param name="collection">String collection to be counted</param>
+        /// <returns>Hashtable where both Key and Vaue are Objects</returns>
+        public static Hashtable CreateWordsHashTable(IEnumerable<string> collection)
+        {
+            Hashtable wordsTable = new Hashtable(collection.Count());
+
+            foreach (string word in collection)
+            {
+                if (wordsTable.ContainsKey(word))
+                {
+                    int current = Convert.ToInt32(wordsTable[word]);
+                    wordsTable[word] = ++current;
+                }
+                else
+                {
+                    wordsTable.Add(word, 1);
+                }
+            }
+            return wordsTable;
+        }
+    }
+}
---
+++ Word_CounterTests/WordCounterTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Word_Counter;
+using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Word_Counter.Tests
+{
+    [TestClass()]
+    public class WordCounterTests
+    {
+        [TestMethod()]
+        public void SplitTextTest()
+        {
+            string text = "My text. text1 word-to-word - ";
+            string[] expected = { "my", "text", "text", "word-to-word" };
+
+            string[] result = WordCounter.SplitText(text).ToArray();
+
+            Assert.AreEqual(expected.Length, result.Length);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], result[i]);
+            }
+        }
+
+        [TestMethod()]
+        public void CreateWordsHashTableTest()
+        {
+            string[] collection = { "one", "one", "two", "four", "two" };
+            Hashtable expectedTable = new Hashtable();
+            expectedTable.Add("one", 2);
+            expectedTable.Add("two", 2);
+            expectedTable.Add("four", 1);
+
+            Hashtable resultTable = WordCounter.CreateWordsHashTable(collection);
+
+            foreach (var key in expectedTable.Keys)
+            {
+                Assert.AreEqual(expectedTable[key], resultTable[key]); 
+            }
+        }
+    }
+}
